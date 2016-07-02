36e899d4321c4bc99edea7a0f0fd8ad3e88a3094
tcc5102 <tyler.cooper91@gmail.com>
2016-07-02 18:21:47 -0400

    Chk 17 Completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate =
+//#1
+var buildCollectionItemTemplate = function() {
+    var template =
     '<div class="collection-album-container column fourth">'
   + '  <img src="assets/images/album_covers/01.png"/>'
   + '  <div class="collection-album-info caption">'
@@ -12,15 +14,20 @@ var collectionItemTemplate =
   + '    </p>'
   + '  </div>'
   + '</div>'
-  ;
-window.onload = function() {
-  // #1
-  var collectionContainer = document.getElementsByClassName('album-covers')[0];
-  // #2
-  collectionContainer.innerHTML = '';
-
-  // #3
-  for (var i = 0; i < 12; i++) {
-    collectionContainer.innerHTML += collectionItemTemplate;
-  }
+    ;
+    //#2
+    return $(template);
 };
+
+$(window).load(function() {
+  // #3
+  var $collectionContainer = $('.album-covers');
+  // #4
+  $collectionContainer.empty();
+
+  for (var i = 0; i < 12; i++) {
+    var $newThumbnail = buildCollectionItemTemplate();
+    //#5
+    $collectionContainer.append($newThumbnail);
+  }
+});
